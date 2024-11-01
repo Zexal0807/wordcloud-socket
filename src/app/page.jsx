59310c0c8f139529bbb9fc98ce0dab9c11ce0abc
Gallina Roberto <gallinar00@gmail.com>
2024-11-01 17:56:08 +0100

    Install bootstrap

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
@@ -19,17 +18,58 @@ export default function Home() {
 		}
 	}, [searchParams]);
 
-	return (
-		<div style={{ padding: "20px" }}>
-			<h1>Word Cloud Realtime</h1>
-			<nav>
-				<Link href="/sender/12345678" style={{ marginRight: "20px" }}>
-					Vai a Sender 12345678
-				</Link>
-				<Link href="/viewer/12345678">Vai a Viewer 12345678</Link>
-			</nav>
+	const [code, setCode] = useState("");
 
-			{queryMessage}
+	return (
+		<div
+			className="w-100 h-100"
+			style={{backgroundColor: "#e491da"}}
+		>
+			<div
+				className="w-100 p-2"
+				style={{
+					backgroundColor: "#550000",
+					color: "white",
+				}}
+			>
+				{queryMessage}
+			</div>
+
+			<div
+				style={{
+					height: "100%",
+					width: "100%",
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+					flexDirection: "column",
+				}}
+			>
+				<h1>MIOQUIZZONE</h1>
+
+				<div
+					style={{
+						backgroundColor: "white",
+						borderRadius: 5,
+						padding: 10,
+					}}
+				>
+					<input
+						style={{ width: "100%", fontSize: 20 }}
+						value={code}
+						onChange={(e) => setCode(e.target.value)}
+					/>
+					<button
+						style={{ width: "100%", backgroundColor: "#e491da" }}
+						onClick={() => {
+							if (code.length != 8) return;
+							router.push(`/sender/${code}`);
+						}}
+					>
+						ENTRA
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 }
