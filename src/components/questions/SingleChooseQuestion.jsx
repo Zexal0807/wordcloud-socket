f6f53d8c6c003601fd9aa9a5acc261b52ab95e71
Zexal0807 <gallinar00@gmail.com>
2025-02-10 14:35:09 +0100

    Create question

--- src/components/questions/SingleChooseQuestion.jsx
+++ src/components/questions/SingleChooseQuestion.jsx
@@ -1,37 +1,60 @@
+import { ResponsiveBar } from "@nivo/bar";
+
 export default function ({ mode, type, question, sendAnswer }) {
+	const { options } = question;
 
-    const { options } = question;
-    debugger;
+	if (type == "sender" && ["quiz","poll"].includes(mode))
+		return (
+			<div className="w-100 h-100 d-flex justify-content-around align-items-center flex-wrap">
+				{options.map((option, key) => (
+					<button
+						key={key}
+						className="col-5 btn btn-success h-25"
+						onClick={() => {
+							sendAnswer(key);
+						}}
+					>
+						{option.text}
+					</button>
+				))}
+			</div>
+		);
+		
+	let { answers } = question;
+	if(answers == undefined)
+		answers = [];
+	let data = [];
+	options.forEach((option) => {
+		data.push({ id: option.text, value: 0 });
+	});
+	answers.forEach((answer) => {
+		data[answer.answer].value++;
+	});
 
-	if (type == "sender" && mode == "quiz") 
-        return (<button
-            onClick={() => {
-                sendAnswer(1);
-            }}
-        >
-            RISPOSTA a
-        </button>
-        );
+	if (type == "viewer" && mode == "quiz")
+		return <div className="w-100 h-100">
+			{answers.length}
+		</div>;
 
-    if (type == "sender" && mode == "poll") 
-        return (<button
-            onClick={() => {
-                sendAnswer(1);
-            }}
-        >
-            RISPOSTA a
-        </button>
-        );
+	if (type == "viewer" && mode == "poll")
+		return (
+			<div className="w-100 h-100">
+				<ResponsiveBar
+					data={data}
+					colorBy="indexValue"
+					theme={{
+						text: {
+							fontSize: 20,
+						}
+					}}
+					margin={{ top: 50, right: 50, bottom: 50, left: 60 }}
+					axisLeft={null}
+					animate={true}
+					motionStiffness={90}
+					motionDamping={15}
+				/>
+			</div>
+		);
 
-	if (type == "viewer" && mode == "quiz") 
-        return (<div className="w-100 h-100">
-            VQ
-        </div>);
-
-    if (type == "viewer" && mode == "poll") 
-        return (<div className="w-100 h-100">
-            VP
-        </div>);
-
-    return <></>;
+	return <></>;
 }
